542902307d885f11dcb312396c8b9c4578a0f33c
fizzday <fizzday@yeah.net>
2016-04-13 09:24:16 +0800

    composer初始化, 创建公共目录及入口文件

---
+++ public/index.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * @Author: anchen
+ * @Date:   2016-04-13 09:07:49
+ * @Last Modified by:   anchen
+ * @Last Modified time: 2016-04-13 09:09:21
+ */
+
+define("PUBLIC_PATH", __DIR__."/");
+
+echo "hello Fizzday";
